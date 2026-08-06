80eded0fc065b5a79c5481c312ae47b2f5d143d4
Jun Dev <jun.dev2001@gmail.com>
2026-08-06 22:47:49 +0700

    build(promotion): wire PromotionService into solution, compose, and gateway

--- scripts/postgres/init.sql
+++ scripts/postgres/init.sql
@@ -9,6 +9,7 @@ CREATE DATABASE inventory_db;
 CREATE DATABASE order_db;
 CREATE DATABASE product_db;
 CREATE DATABASE payment_db;
+CREATE DATABASE promotion_db;
 
 -- Create Hangfire databases for background job processing
 CREATE DATABASE auth_hangfire_db;
