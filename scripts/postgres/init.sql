c854e9fd068e353de22723797fbe030c4161dfd7
Jun Dev <jun.dev2001@gmail.com>
2026-08-06 17:47:17 +0700

    build(payment): wire PaymentService into solution, compose, and gateway

--- scripts/postgres/init.sql
+++ scripts/postgres/init.sql
@@ -8,6 +8,7 @@ CREATE DATABASE user_db;
 CREATE DATABASE inventory_db;
 CREATE DATABASE order_db;
 CREATE DATABASE product_db;
+CREATE DATABASE payment_db;
 
 -- Create Hangfire databases for background job processing
 CREATE DATABASE auth_hangfire_db;
